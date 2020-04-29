46a3dd4f41ee0de830f43a22088ffd692a4c5087
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-04-29 10:15:57 +0700

    :construction: connect API get profile

--- lib/src/screens/profile.dart
+++ lib/src/screens/profile.dart
@@ -1,75 +1,58 @@
+import 'package:base_flutter/src/bloc/profile_bloc.dart';
 import 'package:base_flutter/src/data/constants.dart';
+import 'package:base_flutter/src/models/user.dart';
+import 'package:base_flutter/src/states/user_state.dart';
+import 'package:base_flutter/src/widgets/my_app_toolbar.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
+    ProfileBloc bloc = Provider.of<ProfileBloc>(context, listen: false);
+    bloc.getUser(1);
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          _topPanel(context)
-        ],
-      )
+      appBar: MyAppToolbar(title: 'Profile'),
+      body: _buildBody(context, bloc),
     );
   }
 
-  Widget _topPanel(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      color: Colors.white,
-      padding: EdgeInsets.all(dpConverter(context, 5.0)),
-      child: Stack(
-        children: <Widget>[
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              _userAvatar(context),
-              SizedBox(width: dpConverter(context, 5.0)),
-              Flexible(
-                flex: 1,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    _userFullName(context),
-                    _userEmail(context)
-                  ],
-                ),
-              )
-            ],
-          ),
-          Align(
-            alignment: Alignment.topRight,
-            child: InkWell(
-              child: Text('Detail', style: TextStyle(color: AppColors.grey, fontSize: TextSizeConst.regular)),
-              onTap: () => null,
+  Widget _buildBody(BuildContext context, ProfileBloc bloc) {
+    return StreamBuilder<UserState>(
+      stream: bloc.streamUser,
+      initialData: UserUninitialized(),
+      builder: (context, snapshot) {
+        UserState userState = snapshot.data;
+        if (userState is UserLoading) {
+          return Container(
+            child: Center(
+              child: CircularProgressIndicator(),
             ),
-          )
-        ],
-      )
+          );
+        }
+        if (userState is UserLoaded) {
+          return _buildProfile(context, userState.user);
+        }
+        if (userState is UserError) {
+          return Container(
+            child: Center(
+              child: Text(userState.error.toString()),
+            ),
+          );
+        }
+        return Container();
+      },
     );
   }
 
-  Widget _userAvatar(context) {
-    return CircleAvatar(
-      radius: dpConverter(context, 6.5), 
-      backgroundImage: NetworkImage('https://i.imgur.com/XcTBj1y.png')
-    ); 
-  }
-
-  Widget _userFullName(context) {
-    return  Container(
-      padding: EdgeInsets.only(right: dpConverter(context, 10.0)),
-      child: Text('Michael John', style: TextStyleConst.titleText)
-    );
-      
-  }
-
-  Widget _userEmail(context) {
+  Widget _buildProfile(BuildContext context, User user) {
     return Container(
-      padding: EdgeInsets.only(right: dpConverter(context, 10.0)),
-      child: Text('michaeljohn@gmail.com', style: TextStyle(color: AppColors.grey, fontSize: TextSizeConst.regular)),
+      child: Column(
+        children: <Widget>[
+          Text(user.id.toString(), style: TextStyle(fontSize: dpConverter(context, TextSizeConst.verySmall)))
+        ],
+      ),
     );
   }
 
