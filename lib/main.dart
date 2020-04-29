31999b916079b79947baeda644625bc40d601e83
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-04-29 10:12:07 +0700

    :pencil: enable device preview on debug mode only

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:base_flutter/src/utils/app_helper.dart';
 import 'package:base_flutter/src/my_app.dart';
-
-// void main() => runApp(MyApp());
 
 void main() => runApp(
   DevicePreview(
+    enabled: isInDebugMode ? true : false,
     builder: (context) => MyApp(),
   )
 );
