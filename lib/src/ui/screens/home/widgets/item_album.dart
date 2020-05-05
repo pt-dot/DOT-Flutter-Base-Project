02ee2f4273161254e7e74208bc31a8323826847f
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-05-05 12:26:30 +0700

    :pencil: improve list album

---
+++ lib/src/ui/screens/home/widgets/item_album.dart
@@ -0,0 +1,24 @@
+import 'package:base_flutter/src/core/data/models/album.dart';
+import 'package:flutter/material.dart';
+
+class ItemAlbum extends StatelessWidget {
+
+  const ItemAlbum(this.album);
+
+  final Album album;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(5.0),
+        color: Colors.white,
+      ),
+      child: Center(
+        child: Text(album.title, textAlign: TextAlign.center),
+      ),
+    );
+  }
+
+}
