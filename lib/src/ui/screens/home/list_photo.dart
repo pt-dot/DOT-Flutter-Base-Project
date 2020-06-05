8a9ca7f8944831e743f83cec93bd8647d7f18cf3
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-06-05 08:30:58 +0700

    :fire: remove pagewise library

--- lib/src/ui/screens/home/list_photo.dart
+++ lib/src/ui/screens/home/list_photo.dart
@@ -1,12 +1,10 @@
 import 'package:base_flutter/src/core/bloc/photo_bloc.dart';
-import 'package:base_flutter/src/core/data/constants.dart';
 import 'package:base_flutter/src/core/data/models/album.dart';
 import 'package:base_flutter/src/ui/screens/home/widgets/item_album.dart';
 import 'package:base_flutter/src/ui/shared/base_common_textinput.dart';
 import 'package:base_flutter/src/ui/shared/my_app_toolbar.dart';
 import 'package:base_flutter/src/ui/shared/base_gridview.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_pagewise/flutter_pagewise.dart';
 import 'package:provider/provider.dart';
 
 class ListPhoto extends StatelessWidget {
--- lib/src/ui/screens/home/list_post.dart
+++ lib/src/ui/screens/home/list_post.dart
@@ -1,12 +1,10 @@
 import 'package:base_flutter/src/core/bloc/post_bloc.dart';
-import 'package:base_flutter/src/core/data/constants.dart';
 import 'package:base_flutter/src/core/data/models/post.dart';
 import 'package:base_flutter/src/ui/screens/home/widgets/item_post.dart';
 import 'package:base_flutter/src/ui/shared/base_common_textinput.dart';
 import 'package:base_flutter/src/ui/shared/my_app_toolbar.dart';
 import 'package:base_flutter/src/ui/shared/base_listview.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_pagewise/flutter_pagewise.dart';
 import 'package:provider/provider.dart';
 
 class ListPost extends StatelessWidget {
