da16750e967b71600aedf4f1da5c37f8df0afd2d
Bekti Cahyo <Bektic.22@gmail.com>
2022-10-25 16:48:35 +0700

    remove unused file and library

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -1,24 +1,19 @@
-import 'package:base_flutter/src/core/data/constants.dart';
-import 'package:base_flutter/src/core/providers/provider_list.dart';
 import 'package:base_flutter/src/core/routes/app_route.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-        providers: ProviderList.list,
-        child: MaterialApp(
-          title: 'Flutter Base',
-          locale: DevicePreview.locale(context),
-          builder: DevicePreview.appBuilder,
-          theme: ThemeData(
-            primaryColor: Colors.teal,
-            fontFamily: 'Product-Sans',
-          ),
-          onGenerateRoute: AppRoute.routes,
-        ));
+    return MaterialApp(
+      title: 'Flutter Base',
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      theme: ThemeData(
+        primaryColor: Colors.teal,
+        fontFamily: 'Product-Sans',
+      ),
+      onGenerateRoute: AppRoute.routes,
+    );
   }
 }
