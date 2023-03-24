8deae5e5d10f899ce397083e214559c095088bd4
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-03-24 15:56:18 +0700

    :lipstick: update layout item post

--- lib/src/ui/module/widgets/item_post.dart
+++ lib/src/ui/module/widgets/item_post.dart
@@ -1,6 +1,11 @@
 import 'package:base_flutter/src/core/data/models/post.dart';
 import 'package:flutter/material.dart';
 
+const style = TextStyle(
+  color: Colors.black,
+  fontSize: 16,
+);
+
 class ItemPost extends StatelessWidget {
   const ItemPost({this.post});
 
@@ -8,14 +13,22 @@ class ItemPost extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Padding(
       padding: EdgeInsets.symmetric(vertical: 12, horizontal: 15),
-      child: Text(
-        '${post?.id}. ${post?.title}',
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 16,
-        ),
+      child: Row(
+        children: [
+          Text(
+            '${post?.id ?? 0}',
+            style: style,
+          ),
+          SizedBox(width: 8),
+          Flexible(
+            child: Text(
+              post?.title ?? '',
+              style: style,
+            ),
+          ),
+        ],
       ),
     );
   }
