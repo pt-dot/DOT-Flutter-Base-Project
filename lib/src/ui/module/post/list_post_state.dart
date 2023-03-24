24b519174c74e3030462fb44402160e0c0f4fdbe
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-03-24 09:03:11 +0700

    :pencil: update state

--- lib/src/ui/module/post/list_post_state.dart
+++ lib/src/ui/module/post/list_post_state.dart
@@ -1,30 +1,32 @@
 import 'package:base_flutter/src/core/data/models/post.dart';
 import 'package:equatable/equatable.dart';
 
-enum PostStatus {
-  initial, success, failure
-}
+enum PostStatus { initial, success, failure }
 
 class ListPostState extends Equatable {
   final List<Post> posts;
   final PostStatus status;
   final int page;
+  final bool hasReachedMax;
 
   const ListPostState({
     this.posts = const <Post>[],
     this.status = PostStatus.initial,
     this.page = 1,
+    this.hasReachedMax = false,
   });
 
   ListPostState copyWith({
     List<Post>? posts,
     PostStatus? status,
     int? page,
+    bool? hasReachedMax,
   }) =>
       ListPostState(
         posts: posts ?? this.posts,
         status: status ?? this.status,
         page: page ?? this.page,
+        hasReachedMax: hasReachedMax ?? this.hasReachedMax,
       );
 
   @override
@@ -32,6 +34,7 @@ class ListPostState extends Equatable {
         posts,
         status,
         page,
+        hasReachedMax,
       ];
 
   @override
