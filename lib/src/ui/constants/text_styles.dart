bf677f3899d78f1cef1e4796c175a627530f0571
Aufa Shiddiq <m.aufashiddiq@gmail.com>
2023-03-28 13:30:40 +0700

    add text style & size constant

---
+++ lib/src/ui/constants/text_styles.dart
@@ -0,0 +1,165 @@
+import 'package:base_flutter/src/ui/constants/text_sizes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppTextStyle {
+  static const defaultTextColor = Colors.black;
+
+  static TextStyle extralightStyle = TextStyle(
+      color: defaultTextColor,
+      fontSize: TextSize.regular.sp,
+      fontWeight: FontWeight.w200,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  static TextStyle tintStyle = TextStyle(
+      color: defaultTextColor,
+      fontSize: TextSize.regular.sp,
+      fontWeight: FontWeight.w300,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  static TextStyle regularStyle = TextStyle(
+      color: defaultTextColor,
+      fontSize: TextSize.regular.sp,
+      fontWeight: FontWeight.w400,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  static TextStyle mediumStyle = TextStyle(
+      color: defaultTextColor,
+      fontSize: TextSize.regular.sp,
+      fontWeight: FontWeight.w500,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  static TextStyle semiBoldStyle = TextStyle(
+      color: defaultTextColor,
+      fontSize: TextSize.regular.sp,
+      fontWeight: FontWeight.w600,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  static TextStyle boldStyle = TextStyle(
+      color: defaultTextColor,
+      fontSize: TextSize.regular.sp,
+      fontWeight: FontWeight.w700,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  static TextStyle extraBoldStyle = TextStyle(
+      color: defaultTextColor,
+      fontSize: TextSize.regular.sp,
+      fontWeight: FontWeight.w800,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  // text style start display
+  static TextStyle display1 = TextStyle(
+      color: defaultTextColor,
+      fontSize: 40.sp,
+      fontWeight: FontWeight.w700,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  static TextStyle display2 = TextStyle(
+      color: defaultTextColor,
+      fontSize: 32.sp,
+      fontWeight: FontWeight.w700,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  // text style start heading
+  static TextStyle heading1 = TextStyle(
+      color: defaultTextColor,
+      fontSize: 28.sp,
+      fontWeight: FontWeight.w700,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  static TextStyle heading2 = TextStyle(
+      color: defaultTextColor,
+      fontSize: 24.sp,
+      fontWeight: FontWeight.w700,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  static TextStyle heading3 = TextStyle(
+      color: defaultTextColor,
+      fontSize: 22.sp,
+      fontWeight: FontWeight.w700,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  static TextStyle heading4 = TextStyle(
+      color: defaultTextColor,
+      fontSize: 18.sp,
+      fontWeight: FontWeight.w700,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  static TextStyle heading5ExtraBold = TextStyle(
+      color: defaultTextColor,
+      fontSize: TextSize.medium.sp,
+      fontWeight: FontWeight.w800,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  static TextStyle heading5Bold = TextStyle(
+      color: defaultTextColor,
+      fontSize: TextSize.medium.sp,
+      fontWeight: FontWeight.w700,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  static TextStyle heading5SemiBold = TextStyle(
+      color: defaultTextColor,
+      fontSize: TextSize.medium.sp,
+      fontWeight: FontWeight.w600,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  // text style start body
+  static TextStyle body1 = TextStyle(
+      color: defaultTextColor,
+      fontSize: TextSize.medium.sp,
+      fontWeight: FontWeight.w500,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  static TextStyle body2SemiBold = TextStyle(
+      color: defaultTextColor,
+      fontSize: TextSize.regular.sp,
+      fontWeight: FontWeight.w600,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  static TextStyle body2Medium = TextStyle(
+      color: defaultTextColor,
+      fontSize: TextSize.regular.sp,
+      fontWeight: FontWeight.w500,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  static TextStyle body2 = TextStyle(
+      color: defaultTextColor,
+      fontSize: TextSize.regular.sp,
+      fontWeight: FontWeight.w400,
+      height: 1.5,
+      letterSpacing: 0.2);
+
+  // text style start caption
+  static TextStyle caption1 = TextStyle(
+      color: defaultTextColor,
+      fontSize: TextSize.small.sp,
+      fontWeight: FontWeight.w500,
+      height: 1.5,
+      letterSpacing: 0.3);
+
+  static TextStyle caption2 = TextStyle(
+      color: defaultTextColor,
+      fontSize: TextSize.superSmall.sp,
+      fontWeight: FontWeight.w600,
+      height: 1.5,
+      letterSpacing: 0.3);
+}
