c8a8268984867a00a4a458458b2c8c6ab8738474
Ya'suf Dany <yasfdany@gmail.com>
2023-04-26 15:37:53 +0700

    Automate Image & Icon Constants using vscode plugin studio-cloud.asset-syncronation

---
+++ lib/r.dart
@@ -0,0 +1,27 @@
+class AssetImages {
+	static const String flatava = 'assets/images/flatava.png';
+	static const String logo = 'assets/images/logo.svg';
+	static const String profilePic = 'assets/images/profile_pic.png';
+}
+
+class AssetIcons {
+	static const String icAddCircle = 'assets/icons/ic_add_circle.svg';
+	static const String icAddStory = 'assets/icons/ic_add_story.svg';
+	static const String icAttachment = 'assets/icons/ic_attachment.svg';
+	static const String icCancel = 'assets/icons/ic_cancel.svg';
+	static const String icComment = 'assets/icons/ic_comment.svg';
+	static const String icEdit = 'assets/icons/ic_edit.svg';
+	static const String icExplore = 'assets/icons/ic_explore.svg';
+	static const String icFacebook = 'assets/icons/ic_facebook.svg';
+	static const String icGoogle = 'assets/icons/ic_google.svg';
+	static const String icHome = 'assets/icons/ic_home.svg';
+	static const String icLove = 'assets/icons/ic_love.svg';
+	static const String icLoveGradient = 'assets/icons/ic_love_gradient.svg';
+	static const String icMenu = 'assets/icons/ic_menu.svg';
+	static const String icMoreVertical = 'assets/icons/ic_more_vertical.svg';
+	static const String icRemove = 'assets/icons/ic_remove.svg';
+	static const String icSearch = 'assets/icons/ic_search.svg';
+	static const String icSend = 'assets/icons/ic_send.svg';
+	static const String icTrash = 'assets/icons/ic_trash.svg';
+}
+
