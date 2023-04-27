a12e1ea6ee0c6677c2fb6e2646d99637bf472eaa
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-04-27 09:29:11 +0700

    :art: change font family

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
       builder: DevicePreview.appBuilder,
       theme: ThemeData(
         primarySwatch: AppColors.appBlue,
-        fontFamily: 'Product-Sans',
+        fontFamily: 'Montserrat',
       ),
       onGenerateRoute: AppRoute.routes,
     );
