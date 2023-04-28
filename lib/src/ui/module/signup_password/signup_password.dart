a79dfa5c8fe1f71438bbfc5a6b9b9f7a5fc7b9a9
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-04-28 15:39:35 +0700

    :pencil: sign up password

--- lib/src/ui/module/signup_password/signup_password.dart
+++ lib/src/ui/module/signup_password/signup_password.dart
@@ -1,5 +1,10 @@
+import 'package:base_flutter/src/ui/shared/app_title.dart';
+import 'package:base_flutter/src/ui/shared/base_common_textinput.dart';
 import 'package:base_flutter/src/ui/shared/bottom_language.dart';
+import 'package:base_flutter/src/ui/shared/primary_button.dart';
 import 'package:base_flutter/src/ui/styles/colors.dart';
+import 'package:base_flutter/src/ui/styles/sizes.dart';
+import 'package:base_flutter/src/ui/styles/styles.dart';
 import 'package:flutter/material.dart';
 
 class SignUpPasswordScreen extends StatefulWidget {
@@ -13,14 +18,95 @@ class SignUpPasswordScreen extends StatefulWidget {
 
 class _SignUpPasswordScreenState extends State<SignUpPasswordScreen> {
 
+  TextEditingController textControllerPassword = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
-      body: Center(
-        child: Text('Signup Password'),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              SizedBox(height: 15),
+              Center(
+                child: AppTitle(fontSize: 64),
+              ),
+              SizedBox(height: 51),
+              _createPassword(),
+              SizedBox(height: 3),
+              _requirementPassword(),
+              SizedBox(height: 22),
+              _inputPassword(),
+              SizedBox(height: 16),
+              _buildButton(),
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar: BottomLanguage(),
     );
   }
+
+  Widget _createPassword() {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: MarginSize.defaultMargin,
+      ),
+      child: Text(
+        'Create a password',
+        style: AppTextStyle.mediumStyle.copyWith(
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+
+  Widget _requirementPassword() {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: MarginSize.defaultMargin,
+      ),
+      child: Text(
+        'For security, your password must be six characters or more.',
+        style: AppTextStyle.mediumStyle.copyWith(
+          fontSize: TextSize.superSmall,
+          color: lineColor,
+        ),
+      ),
+    );
+  }
+
+  Widget _inputPassword() {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: MarginSize.defaultMargin,
+      ),
+      child: BaseCommonTextInput(
+        textFieldController: textControllerPassword,
+        label: 'Password',
+        textInputType: TextInputType.visiblePassword,
+      ),
+    );
+  }
+
+  Widget _buildButton() {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: MarginSize.defaultMargin,
+      ),
+      child: SizedBox(
+        width: double.infinity,
+        height: 42,
+        child: PrimaryButton(
+          onPress: () {
+
+          },
+          title: 'Next',
+        ),
+      ),
+    );
+  }
+
 }
