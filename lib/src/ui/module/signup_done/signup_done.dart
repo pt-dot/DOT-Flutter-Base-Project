6dd20785793e6b411fcc9a4f0dea4b7e9442a788
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-04-28 16:13:31 +0700

    :sparkles: sign up done

--- lib/src/ui/module/signup_done/signup_done.dart
+++ lib/src/ui/module/signup_done/signup_done.dart
@@ -1,5 +1,10 @@
+import 'package:base_flutter/src/ui/module/home/home.dart';
+import 'package:base_flutter/src/ui/shared/app_title.dart';
 import 'package:base_flutter/src/ui/shared/bottom_language.dart';
+import 'package:base_flutter/src/ui/shared/primary_button.dart';
 import 'package:base_flutter/src/ui/styles/colors.dart';
+import 'package:base_flutter/src/ui/styles/sizes.dart';
+import 'package:base_flutter/src/ui/styles/styles.dart';
 import 'package:flutter/material.dart';
 
 class SignUpDoneScreen extends StatefulWidget {
@@ -16,11 +21,86 @@ class _SignUpDoneScreenState extends State<SignUpDoneScreen> {
     return Scaffold(
       backgroundColor: backgroundColor,
       body: SafeArea(
-        child: Center(
-          child: Text('Signup Done'),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              SizedBox(height: 15),
+              Center(
+                child: AppTitle(fontSize: 64),
+              ),
+              SizedBox(height: 99),
+              _welcome(),
+              SizedBox(height: 23),
+              _buildButton(),
+              SizedBox(height: 17),
+              _completeRegister(),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomLanguage(),
+    );
+  }
+
+  Widget _welcome() {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: MarginSize.defaultMargin,
+      ),
+      child: Column(
+        children: [
+          Text(
+            'Welcome to Picspile',
+            style: AppTextStyle.mediumStyle.copyWith(
+              fontSize: TextSize.semiLarge,
+            ),
+          ),
+          Text(
+            'david.william',
+            style: AppTextStyle.mediumStyle.copyWith(
+              fontSize: TextSize.semiLarge,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButton() {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: MarginSize.defaultMargin,
+      ),
+      child: SizedBox(
+        width: double.infinity,
+        height: 42,
+        child: PrimaryButton(
+          onPress: () {
+            Navigator.pushNamedAndRemoveUntil(
+              context,
+              Home.routeName,
+              ModalRoute.withName('/'),
+            );
+          },
+          title: 'Complete Sign-up',
+        ),
+      ),
+    );
+  }
+
+  Widget _completeRegister() {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: MarginSize.defaultMargin,
+      ),
+      child: Text(
+        'Add New Phone Number or Email Address',
+        style: AppTextStyle.semiBoldStyle.copyWith(
+          fontSize: 10,
+          color: lineColor,
+        ),
+      ),
     );
   }
 }
